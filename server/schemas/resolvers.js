754ce411af185c67b39e1e250f116c7fde4044b9
MJB <matt.berti13@gmail.com>
2022-07-05 18:20:17 -0500

    added resolvers for login and addUser mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require('apollo-server-express');
+
 const { User, Thought } = require('../models');
 
 
@@ -28,6 +30,28 @@ const resolvers = {
             .populate('friends')
             .populate('thoughts');
         },
+        },
+        Mutation:{
+            addUser: async (parent, args) =>{
+                const user = await User.create(args);
+
+                return user;
+            },
+            login: async (parent, { email, password}) =>{
+                const user = await User.findOne({ email });
+
+                if(!user){
+                    throw new AuthenticationError('Incorrect credentials');
+                }
+
+                const correctPw = await user.isCorrectPassword(password);
+
+                if(!correctPw){
+                    throw new AuthenticationError('Incorrect credentials')
+                }
+
+                return user;
+            }
         }
     };
 
