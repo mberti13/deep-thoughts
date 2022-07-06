badfffa680eb89c908a48d7da56e7c567e66ba1b
MJB <matt.berti13@gmail.com>
2022-07-06 13:18:24 -0500

    added profile logic but wont show up

--- client/src/App.js
+++ client/src/App.js
@@ -43,12 +43,13 @@ function App() {
                 path="/signup"
                 element={<Signup />}
               />
+              <Route path="/profile">
+                <Route path=":username" element={<Profile></Profile>}/>
+                <Route path ="" element={<Profile></Profile>}/>
+              </Route>
+
               <Route
-                path="/profile"
-                element={<Profile />}
-              />
-              <Route
-                path="/thought"
+                path="/thought/:id"
                 element={<SingleThought />}
               />
               <Route path='*'
