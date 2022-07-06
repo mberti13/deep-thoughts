86743465ca961eb86f09558585973e74e666dce2
MJB <matt.berti13@gmail.com>
2022-07-06 14:16:18 -0500

    added jwt decode and implemented signup and login features

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import { setContext } from '@apollo/client/link/context';
 
 
 import Header from './components/Header';
@@ -18,8 +19,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) =>{
+  const token = localStorage.getItem('id_token');
+  return{
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/client';
+
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+
+  const [addUser, { error }] = useMutation(ADD_USER);
+
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -16,7 +24,20 @@ const Signup = () => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    // use try/catch instead of promises to handle errors
+    try {
+      //execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -56,6 +77,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
